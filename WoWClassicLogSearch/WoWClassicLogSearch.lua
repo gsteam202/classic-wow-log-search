3580e7d984be61b8c00aece9561a97c3d635dd4f
1004576 <jihun202@sk.com>
2023-04-05 17:14:42 +0900

    11

--- WoWClassicLogSearch/WoWClassicLogSearch.lua
+++ WoWClassicLogSearch/WoWClassicLogSearch.lua
@@ -2,28 +2,35 @@
 SLASH_LOGS1 = "/logs"
 SlashCmdList["LOGS"] = function(msg)
   local characterName = msg
-
-  -- Construct the API endpoint URL
-  local url = "https://wowndde.huzy.net/api/logs/character-rank?characterName=" .. characterName
-
-  -- Send a HTTP request to the API endpoint
-  local response = SendHTTPRequest(url)
-
-  -- Parse the JSON response
-  local data = json.decode(response)
-
-  -- response.data.characterData.character.zoneRankings.bestPerformanceAverage
-
-  -- Extract the character's Warcraftlogs score
-  local score = 0
-  if data["characterData"]["character"]["zoneRankings"]["bestPerformanceAverage"] then
-    score = data["characterData"]["character"]["zoneRankings"]["bestPerformanceAverage"]
+  local function isempty(s)
+    return s == nil or s == ''
   end
 
-  -- Display the score to the user
-  if score > 0 then
-    print(characterName .. "의 현재 점수는 " .. score.format("%g",string.format("%.1f",v)) .. "점 입니다.")
+  if isempty(characterName) then
+    print("캐릭터 명을 입력하세요 /logs <캐릭터 명>")
   else
-    print(characterName .. "에 대한 로그를 찾을 수 없어요.")
+    -- Construct the API endpoint URL
+    local url = "https://wowndde.huzy.net/api/logs/character-rank?characterName=" .. characterName
+
+    -- Send a HTTP request to the API endpoint
+    local response = SendHTTPRequest(url)
+
+    -- Parse the JSON response
+    local data = json.decode(response)
+
+    -- response.data.characterData.character.zoneRankings.bestPerformanceAverage
+
+    -- Extract the character's Warcraftlogs score
+    local score = 0
+    if data["characterData"]["character"]["zoneRankings"]["bestPerformanceAverage"] then
+      score = data["characterData"]["character"]["zoneRankings"]["bestPerformanceAverage"]
+    end
+
+    -- Display the score to the user
+    if score > 0 then
+      print(characterName .. "의 현재 점수는 " .. tostring(score).format("%g",string.format("%.1f",v)) .. "점 입니다.")
+    else
+      print(characterName .. "에 대한 로그를 찾을 수 없어요.")
+    end
   end
 end
